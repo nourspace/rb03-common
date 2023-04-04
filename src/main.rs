a7ad1e2f7b6ffd812a12fbc21670b6126da7372a
N Space <1257310+nourspace@users.noreply.github.com>
2023-04-04 17:00:41 +0200

    feat: add learnings ^^

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,179 @@
 fn main() {
-    println!("Hello, world!");
+    shadow();
+    data_types();
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+    expressions();
+    condition_if();
+    condition_let();
+    loop_return();
+    loop_label();
+    while_loop();
+    for_loop();
+}
+
+fn shadow() {
+    println!("Shadow");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    println!("spaces='{spaces}'");
+    let spaces = spaces.len();
+    println!("spaces='{spaces}'");
+}
+
+fn data_types() {
+    println!("data_types");
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("{guess}");
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // Characters
+
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+    // Tuples
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    // Arrays
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+
+    // The array named a will contain 5 elements that will all be set to the value 3 initially. This is the same as writing let a = [3, 3, 3, 3, 3]; but in a more concise way.
+    let a = [3; 5];
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn expressions() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+
+    let x = five();
+    println!("The value of x is: {x}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn condition_if() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn condition_let() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+}
+
+fn loop_return() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn loop_label() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn while_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
